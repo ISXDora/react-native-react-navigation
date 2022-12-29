9ef2355deb0cc82d367f36ec17d8ea7f51c5cc06
ISXDora <isinhasena@gmail.com>
2022-12-29 17:57:59 -0300

    moving between screens e going back

--- App.tsx
+++ App.tsx
@@ -10,21 +10,32 @@
  */
 
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
 
 function HomeScreen() {
+  const {navigate} = useNavigation();
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button title="Go to Details" onPress={() => navigate('Details')} />
     </View>
   );
 }
 function DetailsScreen() {
+  const {navigate, push, goBack, popToTop} = useNavigation();
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Button title="Go to Details... again" onPress={() => push('Details')} />
+      <Button title="Go to Home" onPress={() => navigate('Home')} />
+      <Button title="Go back" onPress={() => goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => popToTop()}
+      />
     </View>
   );
 }
@@ -33,7 +44,7 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Details">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen
           name="Home"
